edfe65c27f4345c41e912a0044144d82f18e924f
selaaulia <selaaulia1307@gmail.com>
2021-03-28 20:32:49 +0700

    Tugas Edit file pada resource/views

--- resources/views/Mahasiswa/Index.blade.php
+++ resources/views/Mahasiswa/Index.blade.php
@@ -1,51 +1,67 @@
 @extends('layout')
+
 @section('content')
- <div class="row">
- <div class="col-lg-12 margin-tb">
- <div class="pull-left mt-2">
- <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
- </div>
- <div class="float-right my-2">
- <a class="btn btnsuccess" href="{{ route('mahasiswas.create') }}"> Input Mahasiswa</a>
- </div>
- </div>
- </div>
- 
- @if ($message = Session::get('success'))
- <div class="alert alert-success">
- <p>{{ $message }}</p>
- </div>
- @endif
- 
- <table class="table table-bordered">
- <tr>
- <th>Nim</th>
- <th>Nama</th>
- <th>Kelas</th>
- <th>Jurusan</th>
- <th>No_Handphone</th>
- <th width="280px">Action</th>
- </tr>
- @foreach ($mahasiswas as $Mahasiswa)
- <tr>
- 
- <td>{{ $Mahasiswa->Nim }}</td>
- <td>{{ $Mahasiswa->Nama }}</td>
- <td>{{ $Mahasiswa->Kelas }}</td>
- <td>{{ $Mahasiswa->Jurusan }}</td>
- <td>{{ $Mahasiswa->No_Handphone }}</td>
- <td>
- <form action="{{ route('mahasiswas.destroy',$Mahasiswa-
->Nim) }}" method="POST">
- 
- <a class="btn btninfo" href="{{ route('mahasiswas.show',$Mahasiswa->Nim) }}">Show</a>
- <a class="btn btnprimary" href="{{ route('mahasiswas.edit',$Mahasiswa->Nim) }}">Edit</a>
- @csrf
- @method('DELETE')
- <button type="submit" class="btn btn-danger">Delete</button>
- </form>
- </td>
- </tr>
- @endforeach
- </table>
+
+    <div class="container mt-5">
+
+        <div class="row justify-content-center align-items-center">
+            <div class="card" style="width: 24rem;">
+                <div class="card-header">
+                    Edit Mahasiswa
+                </div>
+                <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form method="post" action="{{ route('mahasiswa.update', $Mahasiswa->Nim) }}" id="myForm">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="Nim">Nim</label>
+                            <input type="text" name="Nim" class="form-control" id="Nim" value="{{ $Mahasiswa->Nim }}"
+                                aria-describedby="Nim">
+                        </div>
+                        <div class="form-group">
+                            <label for="Nama">Nama</label>
+                            <input type="text" name="Nama" class="form-control" id="Nama" value="{{ $Mahasiswa->Nama }}"
+                                aria-describedby="Nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="Kelas">Kelas</label>
+                            <input type="Kelas" name="Kelas" class="form-control" id="Kelas"
+                                value="{{ $Mahasiswa->Kelas }}" aria-describedby="Kelas">
+                        </div>
+                        <div class="form-group">
+                            <label for="Jurusan">Jurusan</label>
+                            <input type="Jurusan" name="Jurusan" class="form-control" id="Jurusan"
+                                value="{{ $Mahasiswa->Jurusan }}" aria-describedby="Jurusan">
+                        </div>
+                        <div class="form-group">
+                            <label for="tanggalLahir">Tanggal Lahir</label>
+                            <input type="tanggalLahir" name="tanggalLahir" class="form-control" id="tanggalLahir"
+                                value="{{ $Mahasiswa->tanggalLahir }}" aria-describedby="tanggalLahir" placeholder="Year-Month-Day">
+                        </div>
+                        <div class="form-group">
+                            <label for="No_Handphone">No_Handphone</label>
+                            <input type="No_Handphone" name="No_Handphone" class="form-control" id="No_Handphone"
+                                value="{{ $Mahasiswa->No_Handphone }}" aria-describedby="No_Handphone">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" class="form-control" id="email"
+                                value="{{ $Mahasiswa->email }}" aria-describedby="email">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
