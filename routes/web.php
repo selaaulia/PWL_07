3af27703b1cb7d411e33cece0e3c8bdcc41d1c8d
selaaulia <selaaulia1307@gmail.com>
2021-03-28 18:45:49 +0700

    Prak Langkah 5 tambah kode pada route/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('mahasiswa', MahasiswaController::class);
 
 Route::get('/', function () {
     return view('welcome');
