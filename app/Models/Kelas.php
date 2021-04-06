9cc227cae816ff1f1962b23e1b31e72194a4e6b0
selaaulia <selaaulia1307@gmail.com>
2021-04-06 16:41:02 +0700

    Langkah 27 menhgisi kode pada Kelas model untuk mendefinikan kelas memiliki banyak mahasiswa

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+Use App\Model\Mahasiswa;
 
 class Kelas extends Model
 {
     use HasFactory;
+    protected $table='kelas'; //Mendefinikasn bahwa model ini terkait dengan tabel kelas
+
+    public function mahasiswas(){
+        return $this->hasMany(Mahasiswa::class);
+    }
 }
