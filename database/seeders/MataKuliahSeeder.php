331e6eba2abaaee0824355a456464589ea7b23b4
selaaulia <selaaulia1307@gmail.com>
2021-04-07 11:25:21 +0700

    Langkah 5 Mengisi kolom table matakuliah di MataKuliahSeeder

--- database/seeders/MataKuliahSeeder.php
+++ database/seeders/MataKuliahSeeder.php
@@ -13,6 +13,28 @@ class MataKuliahSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $matkul = [
+            [   'nama_matkul' => 'Pemrograman Berbasis Objek',
+                'sks' => 3,
+                'jam' => 6,
+                'semester' => 4,
+            ],
+            [   'nama_matkul' => 'Pemrograman Web Lanjut',
+                'sks' => 3,
+                'jam' => 6,
+                'semester' => 4,
+            ],
+            [   'nama_matkul' => 'Basis Data Lanjut',
+                'sks' => 3,
+                'jam' => 4,
+                'semester' => 4,
+            ],
+            [   'nama_matkul' => 'Praktikum Basis Data Lanjut',
+                'sks' => 3,
+                'jam' => 6,
+                'semester' => 4,
+            ],
+        ];
+        DB::table('matakuliah')->insert($matkul);
     }
 }
