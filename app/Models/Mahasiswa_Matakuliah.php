1991233a8b4eead4af3b06f7645139e30a944e8d
selaaulia <selaaulia1307@gmail.com>
2021-04-07 11:46:11 +0700

    Latihan Langkah 3 pendifinisian pada model relasi tabel Mahasiswa & matakuliah

--- app/Models/Mahasiswa_Matakuliah.php
+++ app/Models/Mahasiswa_Matakuliah.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa_Matakuliah extends Model
 {
     use HasFactory;
+    protected $table = 'mahasiswa_matakuliah';
+    protected $fillable = [
+        'mahasiswa_nim',
+        'kelas_id',
+        'nilai'
+    ];
 }
